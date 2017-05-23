a59521e44f468dc20b4edbd53a0bfc898e466d2c
Corban Mailloux <github@corb.co>
2017-05-22 20:07:20 -0400

    Allow tag filtering

---
+++ js/projects.js
@@ -0,0 +1,36 @@
+function filter() {
+    var tagsToInclude = $("#filter-form").serializeArray().map(function(item) {
+        return item.name;
+    });
+
+    var all_projects = $(".project");
+
+    all_projects.hide();
+
+    all_projects.filter(function() {
+        var projectTags = $(this).attr('data-tags').split("|"); // Get the project's tags
+
+        // Check if the project has any of the chosen tags.
+        return projectTags.some(function(value) {
+            return tagsToInclude.indexOf(value) >= 0;
+        });
+    }).show();
+}
+
+$(document).ready(function () {
+
+    $(":checkbox").change(function() {
+        filter();
+    });
+
+    $("#select-all").click(function() {
+        $(":checkbox").prop("checked", true);
+        $(":checkbox").first().change(); // Force an update.
+    });
+
+    
+    $("#select-none").click(function() {
+        $(":checkbox").prop("checked", false);
+        $(":checkbox").first().change(); // Force an update.
+    });
+});
